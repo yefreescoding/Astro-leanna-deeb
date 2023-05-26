49e380613feea1de4850fb4eb45fda6118fcd254
yefreescoding <yefree13santos@outlook.com>
2023-05-26 19:59:08 +0200

    Project migrate to astro workflow

---
+++ src/Data/navData.js
@@ -0,0 +1,12 @@
+const navData = [
+  {
+    name: "About",
+    link: "/about/",
+  },
+  {
+    name: "Blog",
+    link: "/blog/",
+  },
+];
+
+export default navData;
---
+++ src/Data/socialMediaData.js
@@ -0,0 +1,22 @@
+const socialMediaLinks = [
+  {
+    href: "https://www.instagram.com/leanadeebb/",
+    src: "/logos/instagram.png",
+    alt: "Instagram icon",
+    class: "socials__ig",
+  },
+  {
+    href: "https://twitter.com",
+    src: "/logos/twitter.png",
+    alt: "Twitter Icon",
+    class: "socials__ig",
+  },
+  {
+    href: "https://www.linkedin.com",
+    src: "/logos/linkedin.png",
+    alt: "LinkedIn",
+    class: "socials__ig",
+  },
+];
+
+export default socialMediaLinks;
