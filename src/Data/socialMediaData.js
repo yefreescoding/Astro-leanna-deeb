856d71fdf91b1b1f5ff518490a9aef2546bd4695
yefreescoding <yefree13santos@outlook.com>
2023-05-26 20:45:31 +0200

    saved before going home

--- src/Data/socialMediaData.js
+++ src/Data/socialMediaData.js
@@ -3,19 +3,19 @@ const socialMediaLinks = [
     href: "https://www.instagram.com/leanadeebb/",
     src: "/logos/instagram.png",
     alt: "Instagram icon",
-    class: "socials__ig",
+    class: "nav__icons",
   },
   {
     href: "https://twitter.com",
     src: "/logos/twitter.png",
     alt: "Twitter Icon",
-    class: "socials__ig",
+    class: "nav__icons",
   },
   {
     href: "https://www.linkedin.com",
     src: "/logos/linkedin.png",
     alt: "LinkedIn",
-    class: "socials__ig",
+    class: "nav__icons",
   },
 ];
 
