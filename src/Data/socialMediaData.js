d80b673678a24b098568d64ed33275e096ed3ef1
Yefree Valdez <yefree13santos@outlook.com>
2023-05-28 15:33:31 +0200

    edited header, added img folder

--- src/Data/socialMediaData.js
+++ src/Data/socialMediaData.js
@@ -1,22 +1,22 @@
 const socialMediaLinks = [
   {
-    href: "https://www.instagram.com/leanadeebb/",
-    src: "/logos/instagram.png",
-    alt: "Instagram icon",
-    class: "nav__icons",
+    href: 'https://www.instagram.com/leanadeebb/',
+    src: '/logos/instagram.png',
+    alt: 'Instagram icon',
+    class: 'nav__icons',
   },
-  {
-    href: "https://twitter.com",
-    src: "/logos/twitter.png",
-    alt: "Twitter Icon",
-    class: "nav__icons",
-  },
-  {
-    href: "https://www.linkedin.com",
-    src: "/logos/linkedin.png",
-    alt: "LinkedIn",
-    class: "nav__icons",
-  },
+  // {
+  //   href: "https://twitter.com",
+  //   src: "/logos/twitter.png",
+  //   alt: "Twitter Icon",
+  //   class: "nav__icons",
+  // },
+  // {
+  //   href: "https://www.linkedin.com",
+  //   src: "/logos/linkedin.png",
+  //   alt: "LinkedIn",
+  //   class: "nav__icons",
+  // },
 ];
 
 export default socialMediaLinks;
