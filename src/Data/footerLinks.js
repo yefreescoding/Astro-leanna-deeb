0b9478808211b3863f3806787c7d5808036a7914
Yefree Valdez <yefree13santos@outlook.com>
2023-05-30 01:27:14 +0200

    problem fixed, footer styled

---
+++ src/Data/footerLinks.js
@@ -0,0 +1,40 @@
+const links = [
+  {
+    h4: 'Products',
+    link: '/about/',
+    name: 'App',
+    link2: '/about/',
+    name2: 'App2',
+    link3: '/about/',
+    name3: 'App3',
+  },
+  {
+    h4: 'Products',
+    link: '/about/',
+    name: 'App',
+    link2: '/about/',
+    name2: 'App2',
+    link3: '/about/',
+    name3: 'App3',
+  },
+  {
+    h4: 'Products',
+    link: '/about/',
+    name: 'App',
+    link2: '/about/',
+    name2: 'App2',
+    link3: '/about/',
+    name3: 'App3',
+  },
+  {
+    h4: 'Products',
+    link: '/about/',
+    name: 'App',
+    link2: '/about/',
+    name2: 'App2',
+    link3: '/about/',
+    name3: 'App3',
+  },
+];
+
+export default links;
